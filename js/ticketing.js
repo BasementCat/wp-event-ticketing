9d50eadbddf3ed15fe6babbd13a64791398a6893
Alec <alec@alec.elton-announcemedia.local>
2013-09-19 21:07:03 -0500

    Revert w/gfm changes

--- js/ticketing.js
+++ js/ticketing.js
@@ -44,35 +44,7 @@ jQuery(document).ready(function() {
 
 jQuery(function($) {
 	$("#expireStart").datepicker();
-        $("#expireEnd").datepicker();
-        
-        /*
-         * On page load hide all of the payment gateway forms except the selected
-         * form.
-         * 
-         * @since 1.4
-         */
-        changed();
 });
-
-
-/*
- * Toggles which div is shown for the payment gateway information
- * 
- * @since 1.4
- * @author Ben Lobaugh
- */
-function changed() { 
-    var newGateway = jQuery( '#paymentGateway').val(); // The form to show
-    var divs = jQuery( '#paymentGateway').children(); // A list of all the forms
-  
-    // Loop through and hide each form we do not want displayed
-    divs.each( function( k, div ) {
-        jQuery( '#payment-gateway-settings-' + div.value ).css( 'display', 'none');
-    });
-    
-    // Show the one form the user wants to use
-   jQuery( '#payment-gateway-settings-' + newGateway ).css( 'display', 'block');
-}
-    
-    
+jQuery(function($) {
+	$("#expireEnd").datepicker();
+});
